0f03e484c48ee1d1dd9b872fe6de7d48b1fda586
joanita-51 <nakityojoanita51@gmail.com>
2022-07-08 14:24:59 +0300

    employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import {GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject} from '@syncfusion/ej2-react-grids';
-import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import {GridComponent, ColumnsDirective, ColumnDirective,Page, Search, Inject, Toolbar} from '@syncfusion/ej2-react-grids';
+import { employeesData, contextMenuItems, employeesGrid} from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category='Page' title='Orders'/>
+      <Header category='Page' title='Employees'/>
       <GridComponent
         id='gridcomp'
-        dataSource={ordersData}
+        dataSource={employeesData}
         allowPaging //for pagination
         allowSorting //for sorting
+        toolbar={['Search']} //for search
       >
         <ColumnsDirective>
-          {ordersGrid.map((item,index)=>(
+          {employeesGrid.map((item,index)=>(
             <ColumnDirective key={index} {...item}/>
           ))}
         </ColumnsDirective>
         {/* For putting numbers onto the pagination  */}
-        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]}/>
+        <Inject services={[Search,Page,Toolbar]}/>
 
       </GridComponent>
     </div>
