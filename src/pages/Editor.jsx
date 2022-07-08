4d3710e05398075d4d86c116a45ee667d7836254
joanita-51 <nakityojoanita51@gmail.com>
2022-07-08 16:10:39 +0300

    editor app

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar} from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div>
+      <Header category='App' title='Editor'/>
+      <RichTextEditorComponent>
+        <EditorData/>
+        <Inject services = {[HtmlEditor, Toolbar,Image,Link,QuickToolbar]}/>
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
